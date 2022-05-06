93d01038acc50ca6b86ff14eb07e6e34d9c046d0
Luciano Nooijen <luciano@lucianonooijen.com>
2022-05-07 02:04:21 +0300

    Add Rosetta installing to bootstrap
    Signed-off-by: Luciano Nooijen <luciano@lucianonooijen.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,6 +28,9 @@ osascript -e 'tell application "System Preferences" to quit'
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 # while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "Installing Rosetta"
+softwareupdate --install-rosetta --agree-to-license
+
 ###############################################################################
 # MacOS configuration
 ###############################################################################
